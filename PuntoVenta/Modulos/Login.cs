215eef6356358e32cd9f714e24c953f4478c715c
liz-veronica <87659504+liz-veronica@users.noreply.github.com>
2022-10-14 22:57:34 -0300

    Formulario Login

---
+++ PuntoVenta/Modulos/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PuntoVenta.Modulos
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
